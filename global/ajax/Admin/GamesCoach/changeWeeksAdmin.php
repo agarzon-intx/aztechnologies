29ef7d35e7a57333930dfc33c4e3edc9a7265189
Alfredo Garzon Zamora <aztechnologies@192.168.1.12>
2026-06-21 02:11:06 -0600

    Auto sync 2026-06-21 02:11:06

--- global/ajax/Admin/GamesCoach/changeWeeksAdmin.php
+++ global/ajax/Admin/GamesCoach/changeWeeksAdmin.php
@@ -73,6 +73,7 @@ unset($__i, $__prev, $__base, $__inc, $__app_here);
                     and Equipo_ID in (" . $_SESSION[$Config->getAlias() . 'equipo'] . ")
                 order by 1 asc
                 limit 1;";
+			echo $sql00;
 	$result00 = $Config->query($sql00);
     $count = 0;
     if($result00){
@@ -101,7 +102,7 @@ unset($__i, $__prev, $__base, $__inc, $__app_here);
 			    join $schema.Categorias ca on ca.Categoria_ID = $Category and ca.Torneo_Id = $Season and ca.Calendario_Id = j.Calendario_ID
 			where j.Torneo_ID = $Season and j.Jornada_ID = $currentWeek
 			order by j.Jornada_ID;";
-    //echo $sql;
+    		echo $sql1;
 	$result = $Config->query($sql1);
 	if($result){
 		$totJor = $result->num_rows;
