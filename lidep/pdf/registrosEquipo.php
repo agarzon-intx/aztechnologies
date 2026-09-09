92e03c32eb7e73be4d56983f75f0f9438de257b5
Alfredo Garzon Zamora <aztechnologies@192.168.1.64>
2026-09-09 14:28:34 -0600

    Auto sync 2026-09-09 14:28:31

--- lidep/pdf/registrosEquipo.php
+++ lidep/pdf/registrosEquipo.php
@@ -111,7 +111,7 @@
 				$pdf->SetXY($x+40,$y+46);
 				$pdf->Cell(65 , 5, $row["FechaAlta"], 0, 0 , 'L' , false);
 				// Top-right of card (clear of photo/text); drawn last with white pad in helper.
-				az_pdf_qrcode($pdf, $fgmembersite, $row['Jugador_ID'], $x+70, $y+50, 18, 18);
+				az_pdf_qrcode($pdf, $fgmembersite, $row['Jugador_ID'], $x+80, $y+45, 18, 18);
 				if ($col == 1) {
 					$x = $marginX;
 					$col = 0;
