084b00ae2f59e408fc2ce0054a16f90bac42b592
Alfredo Garzon Zamora <aztechnologies@192.168.1.64>
2026-09-15 15:41:14 -0600

    Auto sync 2026-09-15 15:41:14

---
+++ tools/sql/add-juegos-loadinfo.sql
@@ -0,0 +1,30 @@
+-- Adds Juegos.LoadInfo (basketball game JSON) on all site schemas.
+-- Safe to skip a schema if the column already exists.
+-- Run with a database user that has ALTER privileges (cPanel account owner).
+
+ALTER TABLE `aztechn1_demomina`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `aztechn1_elite`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `aztechn1_huskies`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `aztechn1_lidep`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `aztechn1_nuestrodeporte`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `aztechn1_vollidep`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `aztechn1_voleibolmetepec`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `aztechn1_voleymvp`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `aztechn1_aztflag`.`Juegos`
+  ADD COLUMN `LoadInfo` longtext COLLATE utf8_unicode_ci NULL;
